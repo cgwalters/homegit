02463871e4b0770d27caf5f72d8a6f427eee5fd7
Colin Walters <walters@verbum.org>
2019-04-17 14:33:06 +0000

    config: Add more fish aliases

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -7,6 +7,8 @@ alias make='chrt --idle 0 make -j (getconf _NPROCESSORS_ONLN)'
 # things being *built* as root.
 alias makesudoinstall='make; and rm -rf _install; and make install DESTDIR=(pwd)/_install; and sudo rsync -rlv _install/ /; and rm -rf _install'
 alias mk-autoconf='./autogen.sh --prefix=/usr --libdir=/usr/lib64 --sysconfdir=/etc'
+alias c='cp --reflink=auto'
+alias devshell='sudo runuser -u root -- podman run --net=host --rm -ti --privileged -v {$XDG_RUNTIME_DIR}/keyring:{$XDG_RUNTIME_DIR}/keyring -v /srv:/srv:rslave -v /run/libvirt:/run/libvirt:rslave -v /var/tmp:/var/tmp:rslave -v /srv/walters/containers/home:/home/walters -v /srv/walters/containers/roothome:/var/roothome'
 
 if command -v pazi >/dev/null
     status --is-interactive; and pazi init fish | source
